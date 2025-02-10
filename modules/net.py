c38d5783f93aca76646a6f71ea87ede8e465acf0
Darkshades <henrychibueze774@gmail.com>
2025-02-10 15:12:40 +0100

    relu activation between layers

--- modules/net.py
+++ modules/net.py
@@ -43,10 +43,13 @@ class AudioSegmentationNet(nn.Module):
         in_features = (3 * self.config["mfcc_config"]["melkwargs"]["n_mels"]) + 3
         self.fc = nn.Sequential(
             nn.Linear(in_features, network_config["hidden_layers_config"]["l1"]),
+            nn.ReLU(),
             nn.Linear(network_config["hidden_layers_config"]["l1"], network_config["hidden_layers_config"]["l2"]),
             nn.Dropout(network_config["dropout"]),
+            nn.ReLU(),
             nn.Linear(network_config["hidden_layers_config"]["l2"], network_config["hidden_layers_config"]["l3"]),
             nn.Dropout(network_config["dropout"]),
+            nn.ReLU(),
             nn.Linear(network_config["hidden_layers_config"]["l3"], self.num_classes),
         )
         self.apply(self.xavier_init_weights)
