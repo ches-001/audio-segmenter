1934749c7fa3ba2cecfedd49fefb13307558a980
Darkshades <henrychibueze774@gmail.com>
2025-02-13 22:54:37 +0100

    extra hidden layers

--- modules/net.py
+++ modules/net.py
@@ -42,17 +42,25 @@ class AudioSegmentationNet(nn.Module):
         in_features = (3 * self.config["mfcc_config"]["melkwargs"]["n_mels"]) + 3
         self.fc = nn.Sequential(
             nn.Linear(in_features, network_config["hidden_layers_config"]["l1"]),
-            nn.Sigmoid(),
+            nn.SiLU(),
 
             nn.Linear(network_config["hidden_layers_config"]["l1"], network_config["hidden_layers_config"]["l2"]),
-            nn.Sigmoid(),
+            nn.SiLU(),
             nn.Dropout(network_config["dropout"]),
 
             nn.Linear(network_config["hidden_layers_config"]["l2"], network_config["hidden_layers_config"]["l3"]),
-            nn.Sigmoid(),
+            nn.SiLU(),
             nn.Dropout(network_config["dropout"]),
 
-            nn.Linear(network_config["hidden_layers_config"]["l3"], self.num_classes),
+            nn.Linear(network_config["hidden_layers_config"]["l3"], network_config["hidden_layers_config"]["l4"]),
+            nn.SiLU(),
+            nn.Dropout(network_config["dropout"]),
+
+            nn.Linear(network_config["hidden_layers_config"]["l4"], network_config["hidden_layers_config"]["l5"]),
+            nn.SiLU(),
+            nn.Dropout(network_config["dropout"]),
+
+            nn.Linear(network_config["hidden_layers_config"]["l5"], self.num_classes),
         )
         self.apply(self.xavier_init_weights)
 
